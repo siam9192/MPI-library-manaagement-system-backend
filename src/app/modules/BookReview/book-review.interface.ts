bcb240d00017c52a477c7c97cddda57c6562b4f0
siam9192 <ahsiam999@gmail.com>
2025-05-11 13:47:35 +0600

    Permissions types added

--- src/app/modules/BookReview/book-review.interface.ts
+++ src/app/modules/BookReview/book-review.interface.ts
@@ -4,7 +4,6 @@ import { IModelNecessaryFields } from '../../types/model.type';
 export interface IBookReview extends IModelNecessaryFields {
   student: Types.ObjectId;
   book: Types.ObjectId;
-  borrow:Types.ObjectId,
   rating: number;
   
 }
--- src/app/modules/Permission/permission.interface.ts
+++ src/app/modules/Permission/permission.interface.ts
@@ -1,43 +1,180 @@
-interface IPermission {}
+
 type TStudentPermission = {
-  canBorrow: boolean;
-  canAccessLibrary: boolean;
-  canAccessOnlineCourses: boolean;
-  canSubmitAssignments: boolean;
-  canViewGrades: boolean;
-  canJoinEvents: boolean;
+  canBorrowBook: boolean;
+  canRequestWaveFine: boolean;
+  canEditProfile: boolean;
+  canChangePassword: boolean;
+  canUseSupport: boolean;
+  canConfigureBasicSettings: boolean; 
 };
+
 
 type TLibrarianPermissions = {
   // Book Management
-  canManageBooks: boolean; // Create/update/delete books
-
+  canViewBooks:boolean
+  canAddBooks:boolean,
+  canEditBooks:boolean
+  canDeleteBooks:boolean
   // Circulation
-  canProcessBorrowRequests: boolean; // Borrow requests
-  canProcessReturns: boolean; // Book returns
-  canProcessRenewals: boolean; // Loan extensions
-  canOverrideDueDates: boolean; // Special cases
+  canApproveBorrowRequest: boolean; // Borrow requests
+  canRejectBorrowRequest: boolean; // Book returns
+  canOverrideDueDate: boolean; // Special cases
 
   // Reservations
-  canManageReservations: boolean; // Create/update/cancel
+  canProcessReservations:boolean
 
   // Fines
   canViewFines: boolean; // Read-only access
-  canProcessFines: boolean; // Record payments
+  canReceivedFines: boolean; // Record payments
   canWaiveFines: boolean; // Forgive fines
   canAdjustFines: boolean;
 
   // User Management
   canViewStudents: boolean; // Read student info
-  canManageStudents: boolean; // Update student records
+  canDeleteStudents: boolean; // Update student records
+  canEditStudents:boolean
+  canEditStudentPermissions: boolean; 
+  
+
+  // Notifications
+  canViewStudentNotifications:boolean
+  canEditStudentNotification:boolean
+  canDeleteStudentNotifications:boolean
+  canSendStudentNotification:boolean
+  canReceiveNotifications:boolean
 
   // Reports
   canGenerateBasicReports: boolean;
   canGenerateAdvancedReports: boolean;
 
   // System
+  canChangePassword:boolean
   canUpdateProfile: boolean; // Own profile only
   canConfigureBasicSettings: boolean; // Non-critical settings
+ 
 };
 
-type TAdminPermissions = {};
+type TAdminPermissions = {
+   // Book Management
+  canViewBooks:boolean
+  canAddBooks:boolean,
+  canEditBooks:boolean
+  canDeleteBooks:boolean
+  // Circulation
+  canApproveBorrowRequest: boolean; // Borrow requests
+  canRejectBorrowRequest: boolean; // Book returns
+  canOverrideDueDate: boolean; // Special cases
+
+  // Reservations
+  canProcessReservations:boolean
+
+  // Fines
+  canViewFines: boolean; // Read-only access
+  canReceivedFines: boolean; // Record payments
+  canWaiveFines: boolean; // Forgive fines
+  canAdjustFines: boolean;
+
+  // User Management
+  canViewStudents: boolean; // Read student info
+  canDeleteStudents: boolean; // Update student records
+  canEditStudents:boolean
+  canEditStudentPermissions: boolean; 
+  
+  canViewLibrarians:boolean;
+  canDeleteLibrarians:boolean;
+  canEditLibrarians:boolean
+  canEditLibrarianPermissions: boolean; 
+  
+
+
+  canViewStudentNotifications:boolean
+  canEditStudentNotification:boolean
+  canDeleteStudentNotifications:boolean
+  canSendStudentNotification:boolean
+
+  canViewLibrarianNotifications:boolean
+  canEditLibrarianNotification:boolean
+  canDeleteLibrarianNotifications:boolean
+  canSendLibrarianNotification:boolean
+
+  canReceiveNotifications:boolean
+  
+
+  // Reports
+  canGenerateBasicReports: boolean;
+  canGenerateAdvancedReports: boolean;
+
+  // System
+  canChangePassword:boolean
+  canUpdateProfile: boolean; // Own profile only
+  canConfigureBasicSettings: boolean; // Non-critical settings
+  
+};
+
+
+type TSuperPermissions = {
+   // Book Management
+  canViewBooks:true
+  canAddBooks:true,
+  canEditBooks:true
+  canDeleteBooks:true
+  // Circulation
+  canApproveBorrowRequest: true; // Borrow requests
+  canRejectBorrowRequest: true; // Book returns
+  canOverrideDueDate: true; // Special cases
+
+  // Reservations
+  canProcessReservations:true
+
+  // Fines
+  canViewFines: true; // Read-only access
+  canReceivedFines: true; // Record payments
+  canWaiveFines: true; // Forgive fines
+  canAdjustFines: true;
+
+  // User Management
+  canViewStudents: true; // Read student info
+  canDeleteStudents: true; // Update student records
+  canEditStudents:true
+  canEditStudentPermissions: true; 
+
+  canViewLibrarians:true;
+  canDeleteLibrarians:true;
+  canEditLibrarians:true
+  canEditLibrarianPermissions: true; 
+
+  canViewAdmins:true;
+  canDeleteAdmins:true;
+  canEditAdmins:true
+  canAdminLibrarianPermissions: true; 
+  
+  // Notifications
+  canViewStudentNotifications:true
+  canEditStudentNotification:true
+  canDeleteStudentNotifications:true
+  canSendStudentNotification:true
+
+  canViewLibrarianNotifications:true
+  canEditLibrarianNotification:true
+  canDeleteLibrarianNotifications:true
+  canSendLibrarianNotification:true
+  
+  canViewAdminNotifications:true
+  canEditAdminNotification:true
+  canDeleteAdminNotifications:true
+  canSendAdminNotification:true
+  
+  canReceiveNotifications:true
+
+  
+  // Reports
+  canGenerateBasicReports: true;
+  canGenerateAdvancedReports: true;
+
+  // System
+  canChangePassword:true
+  canUpdateProfile: true; // Own profile only
+  canConfigureBasicSettings: true; // Non-critical settings
+  canConfigureCriticalSettings: boolean; 
+  isEditable:false
+};
